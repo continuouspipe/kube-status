f7a797e9a39c2edbbf2562efda5d9fe94c717fed
Samuel ROZE <samuel.roze@gmail.com>
2017-04-09 18:13:41 +0100

    Display details about pods in a modal

--- ui/app/dashboard/controllers/status.js
+++ ui/app/dashboard/controllers/status.js
@@ -1,10 +1,28 @@
 angular.module('kubeStatus')
-    .controller('ClusterStatusController', function($scope, $remoteResource, $mdColors, StatusFetcher, cluster) {
+    .controller('ClusterStatusController', function($scope, $remoteResource, $mdColors, $mdDialog, StatusFetcher, cluster) {
         $scope.cluster = cluster;
 
         $remoteResource.load('status', StatusFetcher.findByCluster(cluster)).then(function (status) {
             $scope.status = status;
         });
+
+        $scope.showPodDetails = function(namespace, pod) {
+            var scope = $scope.$new();
+            scope.pod = pod;
+            scope.namespace = namespace;
+
+            $mdDialog.show({
+                controller: function($mdDialog, $scope) {
+                    $scope.close = function() {
+                        $mdDialog.cancel();
+                    };
+                },
+                templateUrl: 'dashboard/views/pod/details.html',
+                parent: angular.element(document.body),
+                clickOutsideToClose:true,
+                scope: scope
+            });
+        };
 
         $scope.colour_from_percents = function(percents) {
             return $mdColors.getThemeColor(raw_colour_from_percents(percents));
